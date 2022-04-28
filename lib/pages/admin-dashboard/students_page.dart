1e735252e4e92effdf64dbf73e32f2f114729557
Simon Vermeiren <simonvermeiren2002@hotmail.com>
2022-04-28 19:29:07 +0200

    add csv reader

--- lib/pages/admin-dashboard/students_page.dart
+++ lib/pages/admin-dashboard/students_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class StudentsPage extends StatefulWidget {
   const StudentsPage({Key? key}) : super(key: key);
@@ -8,11 +13,24 @@ class StudentsPage extends StatefulWidget {
 }
 
 class _StudentsPageState extends State<StudentsPage> {
-  int currentindex=0;
+
+  List<List<dynamic>> data = [];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Text("test students"),
     );
+  }
+
+  Widget ShowTable(){
+    loadAsset("csv/testData.csv");
+    return Text("data");
+  }
+
+  loadAsset(filePath) async {
+    final myData = await rootBundle.loadString(filePath);
+    List<List<dynamic>> csvTable = const CsvToListConverter().convert(myData);
+    data = csvTable;
   }
 }
