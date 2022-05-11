9562b4038c0e8d643578f96a4e786073f1d0f993
jeltevdyck <jeltevandyck@hotmail.com>
2022-05-11 23:14:29 +0200

    update

--- lib/models/exam_model.dart
+++ lib/models/exam_model.dart
@@ -1,13 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ExamModel {
-  String? uid;
   String? name;
   Timestamp? startDate;
   Timestamp? endDate;
 
   ExamModel({
-    this.uid,
     this.name,
     this.startDate,
     this.endDate,
@@ -15,7 +13,6 @@ class ExamModel {
 
   factory ExamModel.fromMap(map) {
     return ExamModel(
-      uid: map['uid'],
       name: map['name'],
       startDate: map['startDate'],
       endDate: map['endDate'],
@@ -24,7 +21,6 @@ class ExamModel {
 
   Map<String, dynamic> toMap() {
     return {
-      'uid': uid,
       'name': name,
       'startDate': startDate,
       'endDate': endDate,
