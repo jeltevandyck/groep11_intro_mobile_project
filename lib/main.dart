2ab0a1206172b0b8b5bbd6600924965e572f5421
sonia <soniagupta08@hotmail.com>
2022-05-12 10:58:20 +0200

    add landscape and detection leaving page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:groep11_intro_mobile_project/models/student_exam_model.dart';
 import 'package:groep11_intro_mobile_project/pages/Login-dashboard/student_login_page.dart';
 import 'package:groep11_intro_mobile_project/pages/admin-dashboard/students_page.dart';
@@ -24,11 +25,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    config();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Login page',
       theme: ThemeData(primarySwatch: Colors.red),
       home: StudentLoginPage(),
     );
+  }
+
+  void config() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
   }
 }
